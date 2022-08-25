de57f0242e3a96269a3cd19a5230a5a60c191150
Sasha Omarov <Ascander1@yandex.ru>
2022-08-25 11:42:08 +0300

    lesson5

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import {Route, Routes} from "react-router-dom";
-import Profile from "./Profile";
-import NoMatch from "./NoMatch";
-import ChatList from "./ChatList";
-import ChatView from "./ChatView";
-import Layout from "./Layout";
+import Profile from "./components/Profile";
+import NoMatch from "./components/NoMatch";
+import ChatList from "./components/ChatList";
+import ChatView from "./components/ChatView";
+import Layout from "./components/Layout";
+import {Provider} from "react-redux";
+import store from "./store";
 
 const chatItemList = [
     {id: '333', chatUser: 'Alex'},
@@ -19,17 +21,19 @@ function App() {
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo"/>
                 <p>
-                    My home work 4
+                    My home work 5
                 </p>
             </header>
-            <Routes>
-                <Route path="/" element={<Layout/>}>
-                    <Route index element={<ChatList chatItemList={chatItemList}/>}/>
-                    <Route path="profile" element={<Profile/>}/>
-                    <Route path="/chats/:id" element={<ChatView/>}/>
-                    <Route path="*" element={<NoMatch/>}/>
-                </Route>
-            </Routes>
+            <Provider store={store}>
+                <Routes>
+                    <Route path="/" element={<Layout/>}>
+                        <Route index element={<ChatList chatItemList={chatItemList}/>}/>
+                        <Route path="profile" element={<Profile/>}/>
+                        <Route path="/chats/:id" element={<ChatView/>}/>
+                        <Route path="*" element={<NoMatch/>}/>
+                    </Route>
+                </Routes>
+            </Provider>
         </div>
     );
 }
